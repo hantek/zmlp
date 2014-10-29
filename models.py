6d92dec586f237fc3f4dbe5a489b522ebad1f656
hantek <lin.zhouhan@gmail.com>
2014-10-28 22:39:49 -0400

    updata

--- models.py
+++ models.py
@@ -2,7 +2,11 @@
 This file defines some basic models. No cost is defined here. It should go into
 the training part.
 """
+import theano
+import theano.tensor as T
+import numpy
 
 class AutoEncoder(object):
-
+    def __init__():
+        
 
