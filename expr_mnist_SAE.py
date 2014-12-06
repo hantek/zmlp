112710073d64d9997673047f42403f0a1ba1ef27
Zhouhan LIN <lin.zhouhan@gmail.com>
2014-12-05 22:33:07 -0500

    Update expr_mnist_SAE.py

--- expr_mnist_SAE.py
+++ expr_mnist_SAE.py
@@ -1,94 +1,41 @@
 import os
-import gzip
 import numpy
 import theano
 import theano.tensor as T
-import cPickle
 
 from model import ClassicalAutoencoder
 from classifier import LogisticRegression
 from train import GraddescentMinibatch
-
-
-def load_data(dataset):
-    ''' Loads the dataset
-
-    :type dataset: string
-    :param dataset: the path to the dataset (here MNIST)
-    '''
-
-    #############
-    # LOAD DATA #
-    #############
-
-    # Download the MNIST dataset if it is not present
-    data_dir, data_file = os.path.split(dataset)
-    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
-        import urllib
-        origin = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
-        print 'Downloading data from %s' % origin
-        urllib.urlretrieve(origin, dataset)
-
-    print '... loading data'
-
-    # Load the dataset
-    f = gzip.open(dataset, 'rb')
-    train_set, valid_set, test_set = cPickle.load(f)
-    f.close()
-    # train_set, valid_set, test_set format: tuple(input, target)
-    # input is an numpy.ndarray of 2 dimensions (a matrix)
-    # witch row's correspond to an example. target is a
-    # numpy.ndarray of 1 dimensions (vector)) that have the same length as
-    # the number of rows in the input. It should give the target
-    # target to the example with the same index in the input.
-
-    def shared_dataset(data_xy, borrow=True):
-        """ Function that loads the dataset into shared variables
-
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
-        data_x, data_y = data_xy
-        shared_x = theano.shared(
-            numpy.asarray(data_x, dtype=theano.config.floatX),
-            borrow=borrow
-        )
-        shared_y = theano.shared(
-            numpy.asarray(data_y, dtype='int64'),
-            borrow=borrow
-        )
-        return shared_x, shared_y
-
-    test_set_x, test_set_y = shared_dataset(test_set)
-    valid_set_x, valid_set_y = shared_dataset(valid_set)
-    train_set_x, train_set_y = shared_dataset(train_set)
-
-    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
-            (test_set_x, test_set_y)]
-    return rval
-
-
-
-
-
-
-
+from dataset import load_mnist
 
 
 #############
 # LOAD DATA #
 #############
 
-# datasets = load_data('/home/hantek/data/mnist.pkl.gz')
-datasets = load_data('/data/lisa/data/mnist.pkl.gz')
-
-train_set_x, train_set_y = datasets[0]
-valid_set_x, valid_set_y = datasets[1]
-test_set_x, test_set_y = datasets[2]
-
+train_set, valid_set, test_set = load_mnist()
+train_x, train_y = train_set
+test_x, test_y = test_set
+train_x = theano.shared(
+    value = train_x.astype(theano.config.floatX),
+    name = 'train_x',
+    borrow = True
+)
+train_y = theano.shared(
+    value = train_y.astype('int64'),
+    name = 'train_y',
+    borrow = True
+)
+test_x = theano.shared(
+    value = test_x.astype(theano.config.floatX),
+    name = 'test_x',
+    borrow = True
+)
+test_y = theano.shared(
+    value = test_y.astype('int64'),
+    name = 'test_y',
+    borrow = True
+)
 npy_rng = numpy.random.RandomState(123)
 
 ###############
